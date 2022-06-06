99266e253a86514068fe9febfb6a3f9c1f74bd4e
Pedro Calvo <pedrojcalvo@gmail.com>
2022-06-06 20:21:16 +0200

    arreglando error al conectar el servidor 2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,9 +8,13 @@ const loginController = async(req, res ) => {
 
     const { user_email, user_password } = req.body;
     
-    const sql = 'SELECT * FROM users WHERE user_state=true AND user_email=? ';
+    // const sql = 'SELECT * FROM users WHERE user_state=true AND user_email=? ';
 
-    const userLogged = await dbQuery(sql, [user_email]);
+    // const userLogged = await dbQuery(sql, [user_email]);
+
+    const sql = 'SELECT 1 ';
+
+    const userLogged = await dbQuery(sql);
 
     const passwordCorrect = userLogged[0] == null ? false : await bcryptjs.compare(user_password, userLogged[0].user_password);
 

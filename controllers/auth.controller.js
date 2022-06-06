c73f2c2058cb428fc6b34b419fae53bb890e8843
Pedro Calvo <pedrojcalvo@gmail.com>
2022-06-06 20:34:16 +0200

    arreglando error al conectar el servidor 2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,19 +6,13 @@ const { generateJWT } = require('../helpers/generateJWT');
 
 const loginController = async(req, res ) => {
 
-    const { user_email, user_password } = req.body;
+    const { user_email, user_password } = req.params;
     
-    // const sql = 'SELECT * FROM users WHERE user_state=true AND user_email=? ';
+    const sql = 'SELECT * FROM users WHERE user_state=true AND user_email=? ';
 
-    // const userLogged = await dbQuery(sql, [user_email]);
+    const userLogged = await dbQuery(sql, [user_email]);
 
-    const sql = "SELECT * FROM users WHERE user_state=true AND user_email= 'admin@gmail.com' ";
-
-    const userLogged = await dbQuery(sql);
-
-    // const passwordCorrect = userLogged[0] == null ? false : await bcryptjs.compare(user_password, userLogged[0].user_password);
-
-    const passwordCorrect = true;
+    const passwordCorrect = userLogged[0] == null ? false : await bcryptjs.compare(user_password, userLogged[0].user_password);
 
     if(!userLogged[0] || !passwordCorrect){
         res.status(401).json({

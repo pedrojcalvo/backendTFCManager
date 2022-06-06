846cd74b53dd0026d65056d5d25b9a5271a4cefa
Pedro Calvo <pedrojcalvo@gmail.com>
2022-06-06 20:12:20 +0200

    arreglando error al conectar el servidor 2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -14,21 +14,35 @@ const loginController = async(req, res ) => {
 
     const passwordCorrect = userLogged[0] == null ? false : await bcryptjs.compare(user_password, userLogged[0].user_password);
 
-    if(!userLogged[0] || !passwordCorrect){
+    if(!userLogged[0]){
         res.status(401).json({
             
-            error: 'Usuario o contraseña inválido.'
+            error: 'Usuario no encontrado.', secret: process.env.SECRETKEY
         })
-    }else{
-        const token = await generateJWT(userLogged[0]);
-        
-        res.json({
-            id: userLogged[0].user_id,
-            name: userLogged[0].user_name,
-            role: userLogged[0].user_role,
-            token: token
-        });
     }
+
+    if(!passwordCorrect){
+        res.status(401).json({
+            
+            error: 'contraseña incorrecta.', secret: process.env.SECRETKEY
+        })
+    }
+
+    // if(!userLogged[0] || !passwordCorrect){
+    //     res.status(401).json({
+            
+    //         error: 'Usuario o contraseña inválido.', secret: process.env.SECRETKEY
+    //     })
+    // }else{
+    //     const token = await generateJWT(userLogged[0]);
+        
+    //     res.json({
+    //         id: userLogged[0].user_id,
+    //         name: userLogged[0].user_name,
+    //         role: userLogged[0].user_role,
+    //         token: token
+    //     });
+    // }
 }
 
 module.exports = {

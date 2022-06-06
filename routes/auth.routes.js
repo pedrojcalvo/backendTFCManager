431f283f51482eeef85998b15d9304a6102541db
Pedro Calvo <pedrojcalvo@gmail.com>
2022-06-06 19:19:23 +0200

    arreglando error al conectar el servidor 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,7 @@
 const {Router} = require('express');
 const {check} = require('express-validator');
 const { loginController } = require('../controllers/auth.controller');
+const { customerGet } = require('../controllers/customer.controllers');
 const { validateJWT } = require('../middlewares');
 
 const {validateFields} = require('../middlewares/validateFields');
@@ -9,7 +10,10 @@ const {validateFields} = require('../middlewares/validateFields');
 const router = Router();
 
 router.post('/',
-    loginController
+    loginController,
+    validateJWT
 );
+
+router.get('/', customerGet)
 
 module.exports = router;
